b93f487fd31a7a4b818ca302a8cad0d27c85cec3
beliemun <beliemun@paran.com>
2021-01-22 17:13:51 +0900

    lecture - 77%

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import { actionCreators } from "../store";
 
 const Todo = ({ text, id, deleteTodo }) => {
     return (
         <li id={id}>
-            {text} <button onClick={deleteTodo}>DEL</button>
+            <Link to={`/${id}`}>
+                {text} <button onClick={deleteTodo}>DEL</button>
+            </Link>
         </li>
     );
 }
 
 const mapDispatchToProps = (dispatch, ownProps) => {
-    console.log("1", ownProps);
     return {
         deleteTodo: () => dispatch(actionCreators.deleteTodo(ownProps.id))
     }
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,26 @@
-export default () => "Detail";
+import React from "react";
+import { connect } from "react-redux";
+
+const Detail = ({ toDo }) => {
+    return (
+        <h1>
+            {toDo.text}
+        </h1>
+    )
+}
+
+const mapStateToProps = (state, ownProps) => {
+    const {
+        match:
+        {
+            params: { id }
+        }
+    } = ownProps;
+    return {
+        toDo: state.find((toDo, i) => {
+            return toDo.id === parseInt(id);
+        })
+    }
+}
+
+export default connect(mapStateToProps)(Detail);
